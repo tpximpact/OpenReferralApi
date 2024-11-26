ff294bd71ed79db777393e14621ea12d050ee880
DaveIlesTPX <110977208+DaveIlesTPX@users.noreply.github.com>
2024-11-26 19:11:26 +0000

    Update DockerFile

--- OpenReferralApi/Program.cs
+++ OpenReferralApi/Program.cs
@@ -8,6 +8,7 @@ using OpenReferralApi.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables("ORUK_API_");
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
