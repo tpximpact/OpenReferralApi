aacb291ad8c41a5fadfebbab09760994108a40af
DaveIlesTPX <110977208+DaveIlesTPX@users.noreply.github.com>
2025-01-15 19:33:39 +0000

    Return reason for test profile selection

--- OpenReferralApi/Models/ServiceDetails.cs
+++ OpenReferralApi/Models/ServiceDetails.cs
@@ -5,4 +5,5 @@ public class ServiceDetails
     public string Url { get; set; } = null!;
     public bool IsValid { get; set; }
     public string Profile { get; set; } = null!;
+    public string ProfileReason { get; set; } = null!;
 }
