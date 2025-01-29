d41ba76e2c0318cada9b345c53dc12f378534add
DaveIlesTPX <110977208+DaveIlesTPX@users.noreply.github.com>
2025-01-29 12:22:08 +0000

    Make test.id nullable

--- OpenReferralApi/Models/Test.cs
+++ OpenReferralApi/Models/Test.cs
@@ -4,7 +4,7 @@ public class Test
 {
     public string Name { get; set; } = null!;
     public string Endpoint { get; set; } = null!;
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public string Description { get; set; } = null!;
     public bool Success { get; set; }
     public List<Issue> Messages { get; set; } = null!;
